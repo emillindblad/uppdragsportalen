02a0d29f2b5ac6ff27f0711401e3e39968118d3d
emillindblad <emil@lindblad.tech>
2023-01-24 11:34:00 +0100

    now renders

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,7 @@
 import { type NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
-import mkLogo from "../../public/img"
+import mkLogo from "../../public/img/mk.jpg"
 
 const Login: NextPage = () => {
     return (
@@ -9,11 +9,11 @@ const Login: NextPage = () => {
             <Head>
                 <title>Mottagningskommiten</title>
             </Head>
-            <header className="navbar bg-primary">
+            <header className="navbar bg-mk-blue">
                 <span className="navbar-brand text-white p-2">Mottagningskommittèn</span>
             </header>
             <main>
-                <Image src="img/mk.jpg" className="bg-mk" alt={""} />
+                <Image src={mkLogo} className="bg-mk" alt={""} />
                 <div className="container">
                     <h1 className="p-3 text-center text-secondary fw-bold fs-1">Godmorgon gemene teknolog!</h1>
                     <div className="card p-5">
@@ -26,7 +26,7 @@ const Login: NextPage = () => {
                             </div>
                             <input className="form-control form-control-lg bg-primary text-white" type="text" id="passw" placeholder="lösenord" name="passw"/>
                             <button type="submit" className="form-control btn bg-secondary" formAction="index.html">Logga in</button>
-                            {/*<a style="float:right" href="register.html">Registrera dig</a>*/}
+                            <a className="float-right" href="register.html">Registrera dig</a>
                             <label className="me-1 float-right">Inget konto?</label>
                         </form>
                     </div>
