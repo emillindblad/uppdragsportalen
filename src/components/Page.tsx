dc260ddec865938f4efa28af72aa4f3f5103d163
emillindblad <emil@lindblad.tech>
2023-01-24 14:37:10 +0100

    indentation

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,10 +5,10 @@ interface PageProps {
 const Page = (props: PageProps) => {
     return (
         <div className="flex flex-col items-center w-screen justify-center bg-cover bg-center bg-mk-bg h-screen">
-        <h1 className="m-12 text-mk-yellow text-6xl font-bold drop-shadow-2xl">Godmorgon gemene teknolog!</h1>
-        <div className="w-[60%] h-[70%] bg-white rounded-xl shadow-lg">
-            {props.children}
-        </div>
+            <h1 className="m-12 text-mk-yellow text-6xl font-bold drop-shadow-2xl">Godmorgon gemene teknolog!</h1>
+            <div className="w-[60%] h-[70%] bg-white rounded-xl shadow-lg">
+                {props.children}
+            </div>
         </div>
     )
 };
