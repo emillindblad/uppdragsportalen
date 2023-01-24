bbb85f4976ce83be0260c2aefe410d6849ef26d6
Dadi Andrason <dadi.andrason@gmail.com>
2023-01-24 13:36:42 +0100

    changed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <header className="flex align-middle h-14 py-2 px-4 bg-mk-blue">
+        <header className="fixed top-0 w-screen flex align-middle h-14 py-2 px-4 bg-mk-blue">
             <p className="flex items-center text-white text-xl">Mottagningskommittén</p>
         </header>
     )
