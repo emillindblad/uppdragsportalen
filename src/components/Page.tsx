0fa7482a450ab2d8bb8f30af0deb3348215aea27
Dadi Andrason <dadi.andrason@gmail.com>
2023-01-24 14:04:52 +0100

    updated page css

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -6,8 +6,8 @@ interface PageProps {
 const Page = (props: PageProps) => {
     return (
         <div className="flex flex-col items-center w-screen justify-center bg-cover bg-center bg-mk-bg h-screen"> 
-        <span className="m-12 text-secondary text-7xl fw-bold">Godmorgon gemene teknolog!</span>
-        <div className="w-[60%] h-[70%] bg-white rounded-xl shadow-lg">
+        <span className="m-12 text-mk-yellow text-7xl font-bold drop-shadow-lg">Godmorgon gemene teknolog!</span>
+        <div className="w-[60%] h-[70%] bg-white rounded-xl drop-shadow-lg">
             {props.children}
         </div>
         </div>
