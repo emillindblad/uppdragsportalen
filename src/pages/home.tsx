59fc654e266f876ef3725c8599c8c1f9c0da7daa
angelixa <julia.bockert@outlook.com>
2023-01-24 13:38:57 +0100

    started transitioning to tailwind

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,53 +6,52 @@ import { signIn, signOut, useSession } from "next-auth/react";
 const Home: NextPage = () => {
     return (
         <>
-            <Head>
-                <title>Mottagningskommittén</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </Head>
-            <body>
-                <nav className="container m-2 h-screen w-screen">
-                    <div className="row">
-                        <div className="col-4 bg-primary h-screen">
-                            <div className="pb-5 pt-2 row">
-                                <a href="login.html" className="col-4 ms-2">
-                                <img className="foobar-animate h-[40px]" src="img/logo-new.png" />
-                                </a>
-                                <div className="col-8 w-[60%]">
-                                <h5 className="text-end text-white">Gemene NollK</h5>
-                                <p className="text-end text-white">nollkit@chalmers.it</p>
-                                </div>
-                            </div>
-                            <div className="d-grid gap-2">
-                            <button className="btn btn-primary">
-                                Nolluppdrag
-                            </button>
-                            <button className="btn btn-primary">
-                                Arkiv
-                            </button>
-                            <button className="btn btn-primary ">
-                                Dokument
-                            </button>
+         <Head>
+             <title>Mottagningskommittén</title>
+             <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+            <nav className="container m-2 h-screen w-screen">
+                <div className="row">
+                    <div className="col-span-1 bg-primary h-screen">
+                        <div className="pb-5 pt-2 row">
+                            <a href="login.html" className="col-span-1 ms-2">
+                            <img className="foobar-animate h-[40px]" src="img/logo-new.png" />
+                            </a>
+                            <div className="col-span-2 w-[60%]">
+                            <h5 className="text-end text-white">Gemene NollK</h5>
+                            <p className="text-end text-white">nollkit@chalmers.it</p>
                             </div>
                         </div>
-                        <div className="col-8">
-                        <div> 
-                            <div className="row">
-                                <div className="col-4">
-                                    Nolluppdrag
-                                </div>
-                                <div className="col-4">
-                                    Status
-                                </div>
-                                <div className="col-4">
-                                    Inskickad
-                                </div>
-                            </div>
-                        </div>
+                        <div className="d-grid gap-2">
+                        <button className="btn btn-primary">
+                            Nolluppdrag
+                        </button>
+                        <button className="btn btn-primary">
+                            Arkiv
+                        </button>
+                        <button className="btn btn-primary ">
+                            Dokument
+                        </button>
                         </div>
                     </div>
-                </nav>
-            </body>
+                    <div className="col-span-2">
+                    <div> 
+                        <div className="row">
+                            <div className="col-span-1">
+                                Nolluppdrag
+                            </div>
+                            <div className="col-span-1">
+                                Status
+                            </div>
+                            <div className="col-span-1">
+                                Inskickad
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </nav>
+        
         </>
     );
 };
