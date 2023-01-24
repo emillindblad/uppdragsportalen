705b2a0b9e411f36d34325a8059e35ed03fce138
emillindblad <emil@lindblad.tech>
2023-01-24 11:08:30 +0100

    fixed img import

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import placeholderImg from "../../public/img/logo-new.png"
 
 const Home: NextPage = () => {
     return (
@@ -10,20 +10,19 @@ const Home: NextPage = () => {
                 <title>Mottagningskommittén</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
-            <body>
-                <nav className="container m-2 h-screen w-screen">
-                    <div className="row">
-                        <div className="col-4 bg-primary h-screen">
-                            <div className="pb-5 pt-2 row">
-                                <a href="login.html" className="col-4 ms-2">
-                                <img className="foobar-animate h-[40px]" src="img/logo-new.png" />
-                                </a>
-                                <div className="col-8 w-[60%]">
+            <nav className="container m-2 h-screen w-screen">
+                <div className="row">
+                    <div className="col-4 bg-primary h-screen">
+                        <div className="pb-5 pt-2 row">
+                            <a href="login.html" className="col-4 ms-2">
+                                <Image className="foobar-animate h-[40px]" width="40" src={placeholderImg} alt="" />
+                            </a>
+                            <div className="col-8 w-[60%]">
                                 <h5 className="text-end text-white">Gemene NollK</h5>
                                 <p className="text-end text-white">nollkit@chalmers.it</p>
-                                </div>
                             </div>
-                            <div className="d-grid gap-2">
+                        </div>
+                        <div className="d-grid gap-2">
                             <button className="btn btn-primary">
                                 Nolluppdrag
                             </button>
@@ -33,10 +32,10 @@ const Home: NextPage = () => {
                             <button className="btn btn-primary ">
                                 Dokument
                             </button>
-                            </div>
                         </div>
-                        <div className="col-8">
-                        <div> 
+                    </div>
+                    <div className="col-8">
+                        <div>
                             <div className="row">
                                 <div className="col-4">
                                     Nolluppdrag
@@ -49,10 +48,9 @@ const Home: NextPage = () => {
                                 </div>
                             </div>
                         </div>
-                        </div>
                     </div>
-                </nav>
-            </body>
+                </div>
+            </nav>
         </>
     );
 };
