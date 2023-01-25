78aa97f6242f2daffb2ca79a76b1506c270f0653
angelixa <julia.bockert@outlook.com>
2023-01-25 10:18:27 +0100

    changed back to font optimization with next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,26 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { Inter } from '@next/font/google'
 
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
+
+const inter = Inter({ 
+  subsets: ['latin'], 
+  variable: '--font-inter',})
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
+    <main className={`${inter.variable} font-sans`}>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
+    </main>
   );
 };
 
