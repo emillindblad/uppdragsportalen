08297af3a92199ea7a07f3889a6e0f5e4c712b69
emillindblad <emil@lindblad.tech>
2023-01-31 17:04:16 +0100

    prep for create query

--- src/server/api/routers/uppdragrouter.ts
+++ src/server/api/routers/uppdragrouter.ts
@@ -3,6 +3,12 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 export const uppdragrouter = createTRPCRouter({
     getUppdrag: publicProcedure.query(({ ctx }) => {
         return ctx.prisma.uppdrag.findMany({take: 2})
-    })
+    }),
+
+    //addUppdrag: publicProcedure.query(({ ctx }) => {
+        //return ctx.prisma.uppdrag.create({
+            //data: undefined
+        //})
+    //})
 
 });
