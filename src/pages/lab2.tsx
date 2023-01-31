e442ea9094297fd209d4eaf2b1e72d4cd4a1c506
emillindblad <emil@lindblad.tech>
2023-01-31 16:46:52 +0100

    feat: fetch Uppdrag from database on button click

--- src/pages/lab2.tsx
+++ src/pages/lab2.tsx
@@ -1,20 +1,21 @@
+import type { Uppdrag } from "@prisma/client";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 
 import { api } from "../utils/api";
 
 const Lab2Api: NextPage = () => {
-    const [uppdrag, setUppdrag] = useState<string | undefined>("");
+    const [uppdragData, setUppdragData] = useState<Uppdrag[] | undefined>();
 
     const { data, refetch } = api.uppdrag.getUppdrag.useQuery(undefined, {
         refetchOnWindowFocus: false,
-        enabled: true
+        enabled: false
     });
 
     useEffect(() => {
-        setUppdrag("")
+        setUppdragData(data)
     },[data]);
 
     const handleClick = () => { void refetch() };
@@ -27,11 +28,20 @@ const Lab2Api: NextPage = () => {
             <Navbar/>
             <div className="pt-20 px-8">
                 <button onClick={handleClick} className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Get tasks</button>
-                {data?.map( (data) => {
-                    return ( <p>{data.nollk}</p> )
+
+                {uppdragData?.map( (uppdrag) => {
+                    return (
+                        <div className="flex justify-around" key={uppdrag.id}>
+                            <p>{uppdrag.nollk}</p>
+                            <p>{uppdrag.title}</p>
+                            <p>{uppdrag.desc}</p>
+                            <p>{uppdrag.place}</p>
+                            <p>{uppdrag.time}</p>
+                            <p>{uppdrag.participants}</p>
+                        </div>
+                    )
                     })
                 }
-                <p>{uppdrag}</p>
 
             </div>
 
