f1817fa801edc489eb861c3e26af85e50d732f2f
emillindblad <emil@lindblad.tech>
2023-01-31 22:53:08 +0100

    feat: database seeding

--- src/server/api/routers/uppdragrouter.ts
+++ src/server/api/routers/uppdragrouter.ts
@@ -2,7 +2,7 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const uppdragrouter = createTRPCRouter({
     getUppdrag: publicProcedure.query(({ ctx }) => {
-        return ctx.prisma.uppdrag.findMany({take: 2})
+        return ctx.prisma.uppdrag.findMany()
     }),
 
     //addUppdrag: publicProcedure.query(({ ctx }) => {
