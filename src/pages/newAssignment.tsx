44a6acaecf153130be73b37aef1b31a3bc98b64c
steffi0929 <steffi.gumman@hotmail.com>
2023-01-31 16:00:26 +0100

    added more fields to assignment page

--- src/pages/newAssignment.tsx
+++ src/pages/newAssignment.tsx
@@ -11,10 +11,16 @@ const NewAssignment: NextPage = () => {
         <>
             <MainPage title={"Nytt uppdrag"}>
             <div className="my-4 relative h-full">
-                    <textarea id="assignmentTitle" rows={1} className="rounded-2xl w-full block p-3.5 text-2xl bg-white border border-gray-300 focus:ring-mk-blue focus:border-mk-blue resize-none" placeholder="Title"></textarea>
-                    <textarea id="assignmentInfo" className="mt-6 h-[70%] w-full flex-1 rounded-2xl block p-3.5 text-lg bg-white border border-gray-300 focus:ring-mk-blue focus:border-mk-blue resize-none" placeholder="Beskrivning"></textarea>
+                    <input id="assignmentTitle" className="rounded-2xl w-full block p-3.5 text-2xl bg-white border border-gray-300 focus:ring-mk-blue focus:border-mk-blue resize-none" placeholder="Titel" />
+                    <div className="flex mt-6 gap-4">
+                        <input id="assignmentPlace" className="rounded-2xl w-full block p-3.5 text-2xl bg-white border border-gray-300 focus:ring-mk-blue focus:border-mk-blue resize-none" placeholder="Plats" />
+                        <input id="assignmentTime" className="rounded-2xl w-full block p-3.5 text-2xl bg-white border border-gray-300 focus:ring-mk-blue focus:border-mk-blue resize-none" placeholder="Tid" />
+                        <input id="assignmentParticipants" className="rounded-2xl w-full block p-3.5 text-2xl bg-white border border-gray-300 focus:ring-mk-blue focus:border-mk-blue resize-none" placeholder="Antal deltagare" />
+                    </div>
+                    <textarea id="assignmentInfo" className="mt-6 h-[50%] w-full flex-1 rounded-2xl block p-3.5 text-2xl bg-white border border-gray-300 focus:ring-mk-blue focus:border-mk-blue resize-none" placeholder="Beskrivning" />
+                    <textarea id="assignmentMotivation" className="mt-6 h-[15%] w-full flex-1 rounded-2xl block p-3.5 text-2xl bg-white border border-gray-300 focus:ring-mk-blue focus:border-mk-blue resize-none" placeholder="Motivering" />
             </div>
-            <div className="relative bottom-0">
+            <div className="relative bottom-10 ">
                 <div className="absolute bottom-3 left-0">
                     <a href="home">
                         <button className="bg-mk-yellow hover:bg-mk-yellow-hover text-white text-lg rounded-2xl font-bold px-6 py-2" type="button">Tillbaka</button>
