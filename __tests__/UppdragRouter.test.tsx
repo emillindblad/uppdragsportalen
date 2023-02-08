fff1e4744fcc7726babc558c009e7ec858bcc380
emillindblad <emil@lindblad.tech>
2023-02-08 11:23:16 +0100

    removed coverage

--- __tests__/UppdragRouter.test.tsx
+++ __tests__/UppdragRouter.test.tsx
@@ -7,27 +7,23 @@ const ctx =  createInnerTRPCContext({ session: null });
 const caller = appRouter.createCaller(ctx);
 
 test("uppdrag router", async () => {
-
     type Input = inferProcedureInput<AppRouter["example"]["hello"]>;
     const input: Input = {
         text: "test"
     };
 
     const example = await caller.example.hello(input);
-
     expect(example).toMatchObject({ greeting: "Hello test" })
 });
 
 test("get uppdrag from id", async () => {
-
     type Input = inferProcedureInput<AppRouter["uppdrag"]["getUppdragFromId"]>;
     const input: Input = {
         id: '123456789'
     }
 
-    const example = await caller.uppdrag.getUppdragFromId(input);
-
-    expect(example).toMatchObject(
+    const singleUppdrag = await caller.uppdrag.getUppdragFromId(input);
+    expect(singleUppdrag).toMatchObject(
         {
             id: '123456789',
             year: 1969,
@@ -41,4 +37,22 @@ test("get uppdrag from id", async () => {
             private: false
         }
     );
+})
+
+test("fetch all and delete one", async () => {
+    const allUppdrag = await caller.uppdrag.getAllUppdrag();
+
+    const initalLength = allUppdrag.length;
+
+    type Input = inferProcedureInput<AppRouter["uppdrag"]["demoRemoveUppdrag"]>;
+    const input: Input = {
+        nollk: allUppdrag[0]?.nollk as string
+    }
+
+    const deleteOne = await caller.uppdrag.demoRemoveUppdrag(input);
+    const afterDeleteUppdrag = await caller.uppdrag.getAllUppdrag();
+
+    expect(afterDeleteUppdrag.length).lessThan(initalLength);
+
+
 })
