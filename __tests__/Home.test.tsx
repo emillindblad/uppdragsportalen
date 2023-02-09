31d219d9e698a156831b2fb2ef32d59e37deb00d
emillindblad <emil@lindblad.tech>
2023-02-09 13:01:43 +0100

    prepering for react testing

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen, within } from '@testing-library/react'
+import { describe, expect, test } from "vitest";
+import Navbar from '../src/components/Navbar';
+
+test('home', () => {
+    expect(1).equals(1);
+    //render(<Navbar />)
+    //const main = within(screen.getByRole('main'))
+    //expect(
+        //main.getByRole('heading', { level: 1, name: /welcome to next\.js!/i })
+    //).toBeDefined()
+
+    //const footer = within(screen.getByRole('contentinfo'))
+    //const link = within(footer.getByRole('link'))
+    //expect(link.getByRole('img', { name: /vercel logo/i })).toBeDefined()
+})
