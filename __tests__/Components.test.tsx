e20bc3b1f37e6ad5a712e22eaae4e99619d4ce59
steffi0929 <steffi.gumman@hotmail.com>
2023-02-15 09:59:04 +0100

    some more tests

---
+++ __tests__/Components.test.tsx
@@ -0,0 +1,68 @@
+import { render, screen, within } from '@testing-library/react'
+import { describe, expect, test, vi } from "vitest";
+import Navbar from '../src/components/Navbar';
+import SideMenu from '../src/components/SideMenu';
+import SideButton from '../src/components/SideButton';
+import mockRouter from 'next-router-mock'
+import { Uppdrag } from '@prisma/client';
+import AssignmentData from '../src/components/AssignmentData';
+
+vi.mock('next/router', () => require('next-router-mock')); //Needed for componens that use next/router
+
+test('home', () => {
+    render(<Navbar />)
+    expect(screen.getByText( "MOTTAGNINGSKOMMITTÉN")).toBeDefined()
+})
+
+describe("SideMenu isMk=true/false", () => {
+    test("should show buttons for MK all the time", () => {
+        render(<SideMenu isMK = {true}/>);
+        const sideMenu = within(screen.getByRole('navigation'))
+        expect(sideMenu.getByRole('button', { name: /Granska/i })).toBeDefined()
+        expect(sideMenu.getByRole('button', { name: /Konton/i })).toBeDefined()
+    })
+
+    test("should show buttons for NollK all the time", () => {
+        render(<SideMenu isMK = {false}/>);
+        const sideMenu = within(screen.getByRole('navigation'))
+        expect(sideMenu.getByRole('button', { name: /Mina nolluppdrag/i })).toBeDefined()
+        expect(sideMenu.getByRole('button', { name: /Arkiv/i })).toBeDefined()
+    })
+})
+
+describe("SideButtons", () => {
+    test("should show all buttons with right naming", () => {
+        render(<SideButton link={'login'}>
+                Chalmers nolluppdrag
+                </SideButton>);
+        const sideButton = within(screen.getByRole('link'))
+        expect(sideButton.getByRole('button', { name: /Chalmers nolluppdrag/i })).toBeDefined()
+    })
+    // skipping testing the link for now
+})
+
+describe("AssignmentData", () => {
+    test("Should print info about a Uppdrag", () => {
+        const testInfo: Uppdrag[] = [
+            {
+                id: "123",
+                year: 2023,
+                nollk: 'NollKIT',
+                title: 'Hängigt uppdrag',
+                desc: 'Sven hittar säkert på något som blir rent av nekat',
+                place: 'Din mamma',
+                time: '12:01',
+                participants: 2,
+                motivation: 'Hej PR!',
+                private: false
+            }
+        ];
+
+        render(<AssignmentData data={testInfo}/>);
+        const page = within(screen.getByRole('row'));
+        expect(page.getByRole('cell', {name: /NollKIT/i})).toBeDefined();
+        expect(page.getByRole('cell', {name: /Hängigt uppdrag/i})).toBeDefined();
+        expect(page.getByRole('cell', {name: /Sven hittar säkert på något som blir rent av nekat/i})).toBeDefined();
+        expect(page.getByRole('cell', {name: /false/i})).toBeDefined();
+    })
+})
