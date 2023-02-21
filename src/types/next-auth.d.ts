9212d3fbf05cbb54d55251e1c5f640fe288f5589
emillindblad <emil@lindblad.tech>
2023-02-21 17:03:12 +0100

    added adminStatus to session object

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,12 +1,26 @@
-import { type DefaultSession } from "next-auth";
+import { type DefaultSession, type DefaultJWT  } from "next-auth";
 
 declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
-  interface Session {
-    user?: {
+  //interface Session {
+    //user?: {
+      //id: string;
+    //} & DefaultSession["user"];
+  //}
+    //interface JWT {
+        //isAdmin?: boolean | null;
+    //} DefaultJWT
+
+    interface User {
       id: string;
-    } & DefaultSession["user"];
+      name: string;
+      email: string;
+      isAdmin: boolean;
+    }
+  interface Session {
+    user: User,
+    expires: ISODateString
   }
 }
