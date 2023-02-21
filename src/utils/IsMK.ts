361e71dc514d71cca47a4859534e46e8f0dd7d0d
emillindblad <emil@lindblad.tech>
2023-02-21 17:08:29 +0100

    updated ismk to use session object

--- src/utils/IsMK.ts
+++ src/utils/IsMK.ts
@@ -1,10 +1,8 @@
 import { useSession } from 'next-auth/react'
-import { api } from './api'
 
 export const IsMK = () => {
   const { data: session } = useSession()
-  const admins = api.user.getAdmins.useQuery()
-  return admins.data?.some((admin) => admin.id == session?.user?.id)
+  return session?.user.isAdmin;
 }
 
 export default IsMK
