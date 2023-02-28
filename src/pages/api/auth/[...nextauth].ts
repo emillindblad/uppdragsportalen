2e7abd83f70e28ccd98515eb8ef9a06216fe2cb7
Dadi Andrason <dadi.andrason@gmail.com>
2023-02-28 12:31:58 +0100

    added some register stuff

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,7 +12,6 @@ export const authOptions: NextAuthOptions = {
     secret: env.NEXTAUTH_SECRET,
     pages: {
         signIn: "/login",
-        newUser: "/register",
         error: "/login",
     },
     providers: [
--- src/server/api/routers/userrouter.ts
+++ src/server/api/routers/userrouter.ts
@@ -1,47 +1,61 @@
 import { z } from "zod";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import { hashPw } from "../../hash";
 
 export const userrouter = createTRPCRouter({
     getUser: protectedProcedure
-    .input(z.object({ id: z.string() }))
-    .query(({ ctx, input }) => {
-        return ctx.prisma.user.findFirst({
-            where: { id: input.id }
-        });
-    }),
+        .input(z.object({ id: z.string() }))
+        .query(({ ctx, input }) => {
+            return ctx.prisma.user.findFirst({
+                where: { id: input.id }
+            });
+        }),
 
     updateNameEmail: protectedProcedure
-    .input(z.object({ id: z.string(), name: z.string(), email: z.string() }))
-    .mutation(({ ctx, input }) => {
-        return ctx.prisma.user.update({
-            where: { id: input.id },
-            data: {
-                name: input.name,
-                email: input.email
-            }
-        });
-    }),
+        .input(z.object({ id: z.string(), name: z.string(), email: z.string() }))
+        .mutation(({ ctx, input }) => {
+            return ctx.prisma.user.update({
+                where: { id: input.id },
+                data: {
+                    name: input.name,
+                    email: input.email
+                }
+            });
+        }),
 
     updateAllInfo: protectedProcedure
-    .input(z.object({ id: z.string(), name: z.string(), email: z.string(), password: z.string() }))
-    .mutation(async ({ ctx, input }) => {
-        const hashedPassword = await hashPw(input.password)
-        return ctx.prisma.user.update({
-            where: { id: input.id },
-            data: {
-                name: input.name,
-                email: input.email,
-                password: hashedPassword,
-            }
-        });
-    }),
+        .input(z.object({ id: z.string(), name: z.string(), email: z.string(), password: z.string() }))
+        .mutation(async ({ ctx, input }) => {
+            const hashedPassword = await hashPw(input.password)
+            return ctx.prisma.user.update({
+                where: { id: input.id },
+                data: {
+                    name: input.name,
+                    email: input.email,
+                    password: hashedPassword,
+                }
+            });
+        }),
 
     getUserStatus: protectedProcedure
-    .query(({ ctx }) => {
-        return ctx.session.user.isAdmin;
-    }),
+        .query(({ ctx }) => {
+            return ctx.session.user.isAdmin;
+        }),
 
+    registerNewUser: publicProcedure
+        .input(z.object({ name: z.string(), email: z.string(), password: z.string(), nollk: z.string() }))
+        .mutation(async ({ ctx, input }) => {
+            const hashedPassword = await hashPw(input.password)
+            return ctx.prisma.user.create({
+                data: {
+                    name: input.name,
+                    email: input.email,
+                    password: hashedPassword,
+                    nollk: input.nollk,
+                    year: new Date().getFullYear(),
+                }
+            });
+        }),
 });
 
 
