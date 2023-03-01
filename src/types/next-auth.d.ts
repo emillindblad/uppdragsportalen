06b938f273311a04ec302063d072f48cfa3062da
emillindblad <emil@lindblad.tech>
2023-03-01 11:12:20 +0100

    fixed session object

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,27 +1,31 @@
+// This import must exist but still throws a lint error, so it is ignore with the comment bellow
+//
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { type DefaultSession, type DefaultJWT  } from "next-auth";
+
 declare module "next-auth" {
-    /**
-* Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-*/
-    //interface Session {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  //interface Session {
     //user?: {
-    //id: string;
+      //id: string;
     //} & DefaultSession["user"];
-    //}
+  //}
     //interface JWT {
-    //isAdmin?: boolean | null;
+        //isAdmin?: boolean | null;
     //} DefaultJWT
 
     interface User {
-        id: string,
-        name: string,
-        email: string,
-        nollk: string,
-        year: int,
-        isAdmin: boolean,
+      id: string;
+      name: string;
+      email: string;
+      nollk: string;
+      year: int;
+      isAdmin: boolean;
     }
-
-    interface Session {
-        user: User,
-        expires: ISODateString
-    }
+  interface Session {
+    user: User,
+    expires: ISODateString
+  }
 }
