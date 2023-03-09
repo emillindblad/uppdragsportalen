42eee11be85776e9a4d346e63cbd47fdd844d8ed
steffi0929 <steffi.gumman@hotmail.com>
2023-03-09 13:26:36 +0100

    should be mergeable now

--- src/components/AssignmentData.tsx
+++ src/components/AssignmentData.tsx
@@ -1,6 +1,7 @@
 import type { Uppdrag } from "@prisma/client";
 import { useRouter } from "next/router";
 import type { FunctionComponent } from "react";
+import { api } from "../utils/api";
 
 interface UppdragsProps {
     data: Uppdrag[]
@@ -16,15 +17,23 @@ const AssignmentData: FunctionComponent<UppdragsProps> = (props: UppdragsProps) 
         }, '/uppdrag/viewuppdrag')
     }
 
+    // different data depending on user (MK or Nollk)
+    const {data: isMK} = api.user.getUserStatus.useQuery();
+
     return (
         <>
             {props.data?.map( (u) => {
                 return (
-                    <div role="assignments" className="grid grid-cols-4 text-left text-xl hover:bg-gray-300" key={u.id}>
+                    <div role="assignments" className="grid grid-cols-5 text-left text-xl" key={u.id}>
                         <div onClick={() => nav(u.id)} className="flex-initial ml-4 my-2 max-w-[180px] hover:cursor-pointer hover:underline">{u.title}</div>
+                        <div className="flex-initial my-2 ml-1 max-w-[180px]">{u.place}</div>
+                        {isMK ? <div className="flex-initial col-span-1 max-w-[180px]">{u.nollk}</div>
+                              : (<>
+                                    <div className="flex-initial  max-w-[180px]">{u.time}</div>
+                                    <div className="flex-initial  max-w-[180px]">{u.private.toString()}</div>
+                                </>)
+                        }
                         <div className="flex-initial my-2 ml-1 max-w-[180px]">{u.status}</div>
-                        <div className="flex-initial  max-w-[180px]">{u.private.toString()}</div>
-                        <div className="flex-initial my-2 ml-1">{u.place}</div>
 
                         {/*
                         <div className="flex-initial col-span-1 max-w-[180px]">{u.nollk}</div>
