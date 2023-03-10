7a18be14f941bb8d5df4791ffafa3cdd1776dc56
emillindblad <emil@lindblad.tech>
2023-03-10 13:42:33 +0100

    feat: form for granskning

--- src/components/UppdragComment.tsx
+++ src/components/UppdragComment.tsx
@@ -1,40 +1,101 @@
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { type SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+import { UppdragStatus } from "@prisma/client";
+import { ErrorText } from "./ErrorText";
 
-{/* Only for MK */}
+const commentSchema = z.object({
+    comment: z.string().min(2,{ message: "Vänligen skriv in en kommentar" }),
+    status: z.nativeEnum(UppdragStatus)
+});
+
+type FormSchemaType = z.infer<typeof commentSchema>;
+
 const UppdragComment = () => {
+
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<FormSchemaType>({
+        resolver: zodResolver(commentSchema),
+        defaultValues: {
+            //status: 'DRAFT'
+        }
+    });
+
+    const submit: SubmitHandler<FormSchemaType> = (data) => {
+        console.log(data)
+    };
+
+
     return (
         <>
-            <div className="flex row-start-6 col-start-1 col-span-7 items-end px-2">
-                <textarea id="mkComment" className="mt-6 h-[20vh] w-full flex-1 rounded-2xl block p-3.5 text-2xl bg-white border border-gray-300 focus:ring-mk-blue focus:border-mk-blue resize-none" placeholder="Kommentarer.."></textarea>
-            </div>
-            <div className="flex row-start-7 col-start-1 col-end-7 items-end justify-between px-2">
-                <div className="col-start-1 col-span-1">
-                    <Link href="/home">
-                        <button className="h-[44px] w-[125px] bg-mk-yellow hover:bg-mk-yellow-hover text-white text-lg rounded-2xl font-bold px-6 py-2" type="button">Tillbaka</button>
-                    </Link>
-                </div>
-                <div className="flex col-end-7 col-span-4">
-                    <div className="flex justify-between mr-6">
-                        <div className="flex items-center mr-6">
-                            <input type="radio" id="neka" value="" name="default-radio" className="w-4 h-4 text-mk-blue bg-gray-100 border-gray-300 focus:ring-mk-blue"/>
-                            <label htmlFor="neka" className="ml-2 text-lg font-bold text-black">Neka</label>
+            <form action="">
+                <div className="flex flex-col gap-4">
+                    <div className="flex row-start-6 col-start-1 col-span-7 items-end px-2">
+                        <form action=""></form>
+                        <textarea
+                            id="mkComment"
+                            className="h-[15vh] w-full flex-1 rounded-2xl block p-3.5 text-xl bg-white border border-gray-300 focus:ring-mk-blue focus:border-mk-blue resize-none"
+                            placeholder="Kommentarer.."
+                            {...register('comment')}
+                        >
+                        </textarea>
+                    </div>
+                    <div className="flex row-start-7 col-start-1 col-end-7 items-end justify-between px-2">
+                        <div className="flex gap-5">
+                            <Link href="/home">
+                                <button className="h-[44px] w-[125px] bg-mk-yellow hover:bg-mk-yellow-hover text-white text-lg rounded-2xl font-bold px-6 py-2" type="button">Tillbaka</button>
+                            </Link>
+                            {errors.comment?.message && <ErrorText text={errors.comment?.message}/>}
+                            {errors.status?.message && <ErrorText text='Vänligen välj en status på uppdraget'/>}
                         </div>
-                        <div className="flex items-center mr-6">
-                            <input type="radio" id="retur" value="" name="default-radio" className="w-4 h-4 text-mk-blue bg-gray-100 border-gray-300 focus:ring-mk-blue"/>
-                            <label htmlFor="retur" className="ml-2 text-lg font-bold text-black">Retur</label>
-                        </div>
-                        <div className="flex items-center mr-6">
-                            <input type="radio" id="ok" value="" name="default-radio" className="w-4 h-4 text-mk-blue bg-gray-100 border-gray-300 focus:ring-mk-blue"/>
-                            <label htmlFor="ok"className="ml-2 text-lg font-bold text-black">Godkänn</label>
+                        <div className="flex col-end-7 col-span-4">
+                            <div className="flex justify-between mr-6">
+                                <div className="flex items-center mr-6">
+                                    <input
+                                        type="radio"
+                                        value={UppdragStatus.DENIED}
+                                        className="w-4 h-4 text-mk-blue bg-gray-100 border-gray-300 focus:ring-mk-blue"
+                                        {...register('status')}
+                                    />
+                                    <label htmlFor="neka" className="ml-2 text-lg font-bold text-black">Neka</label>
+                                </div>
+                                <div className="flex items-center mr-6">
+                                    <input
+                                        type="radio"
+                                        id="retur"
+                                        value={UppdragStatus.RETURN}
+                                        className="w-4 h-4 text-mk-blue bg-gray-100 border-gray-300 focus:ring-mk-blue"
+                                        {...register('status')}
+                                    />
+                                    <label htmlFor="retur" className="ml-2 text-lg font-bold text-black">Retur</label>
+                                </div>
+                                <div className="flex items-center mr-6">
+                                    <input
+                                        type="radio"
+                                        id="ok"
+                                        value={UppdragStatus.APPROVED}
+                                        className="w-4 h-4 text-mk-blue bg-gray-100 border-gray-300 focus:ring-mk-blue"
+                                        {...register('status')}
+                                    />
+                                    <label htmlFor="ok"className="ml-2 text-lg font-bold text-black">Godkänn</label>
+                                </div>
+                            </div>
+                            <div className="flex">
+                                <Link href="/home">
+                                    <button className="h-[44px] w-[125px] bg-mk-blue hover:bg-mk-blue-hover text-white text-lg rounded-2xl font-bold px-6 py-2"
+                                        type="button"
+                                        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                                        onClick={handleSubmit(submit)}
+                                    >
+                                        Skicka
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
                     </div>
-                    <div className="flex">
-                        <Link href="/home">
-                            <button className="h-[44px] w-[125px] bg-mk-blue hover:bg-mk-blue-hover text-white text-lg rounded-2xl font-bold px-6 py-2" type="button">Skicka</button>
-                        </Link>
-                    </div>
                 </div>
-            </div>
+            </form>
         </>
     );
 };
--- src/pages/uppdrag/viewuppdrag.tsx
+++ src/pages/uppdrag/viewuppdrag.tsx
@@ -35,7 +35,8 @@ const ViewUppdrag: NextPage<Props> = (props: Props) => {
     return (
         <>
             <MainPage session={session} title={uppdrag?.title}>
-                <div className="grid grid-cols-6 grid-rows-[100px_minmax(300px,auto)] h-full pb-6">
+                <div className="flex flex-col grid-cols-6 grid-rows-[100px_minmax(0,auto)] h-full pb-6 justify-between">
+                    <div className="flex flex-col gap-3">
                     <div className="flex col-start-1 col-end-7 row-start-1 row-span-1 border-b-2 border-black p-2 h-20 items-center justify-between">
                         <div className="flex items-start text-4xl font-bold text-left">
                             {uppdrag?.title}
@@ -49,43 +50,73 @@ const ViewUppdrag: NextPage<Props> = (props: Props) => {
                             </div>
                         </div>
                     </div>
-                    <div className="row-start-2 col-start-1 col-end-7">
-                            <div className="text-2xl font-bold text-left px-2"> Antal deltagare: {uppdrag?.participants} </div>
-                            <div className="text-2xl font-bold text-left px-2"> Plats: {uppdrag?.place} </div>
-                            <div className="text-2xl font-bold text-left px-2"> Tid: {uppdrag?.time} </div>
-                            <div className="text-2xl font-bold text-left px-2"> Motivation: {uppdrag?.motivation} </div>
-                            <div className="text-2xl font-bold text-left px-2"> Status: {uppdrag?.status} </div>
-                            <div className="text-2xl font-bold text-left px-2"> Privat: {uppdrag?.private.toString()} </div>
+                    <div className="row-start-2 col-start-1 col-end-7 h-fit">
+                            <div className="text-xl font-bold text-left px-2"> Antal deltagare:
+                                <p className="ml-2 inline font-normal">
+                                    {uppdrag?.participants}
+                                </p>
+                            </div>
+                            <div className="text-xl font-bold text-left px-2"> Plats:
+                                <p className="ml-2 inline font-normal">
+                                    {uppdrag?.place}
+                                </p>
+                            </div>
+                            <div className="text-xl font-bold text-left px-2"> Tid:
+                                <p className="ml-2 inline font-normal">
+                                    {uppdrag?.time}
+                                </p>
+                            </div>
+                            <div className="text-xl font-bold text-left px-2"> Status:
+                                <p className="ml-2 inline font-normal">
+                                    {uppdrag?.status}
+                                </p>
+                            </div>
+                            <div className="text-xl font-bold text-left px-2"> Privat:
+                                <p className="ml-2 inline font-normal">
+                                    {uppdrag?.private.toString()}
+                                </p>
+                            </div>
+                            <div className="text-xl font-bold text-left px-2"> Beskrivning:
+                                <p className="font-normal">
+                                    {uppdrag?.desc}
+                                </p>
+                            </div>
                     </div>
-                    <div className="row-start-3 col-start-1 col-span-7 text-2xl font-bold text-left px-2"> {uppdrag?.desc} </div>
-                    {isMK ? (<UppdragComment/>) :
-                    (<div className="flex gap-9 col-start-1 col-span-2 self-end">
-                        <Link href="/home">
-                            <button
-                                className="h-[44px] w-[125px] bg-mk-yellow hover:bg-mk-yellow-hover text-white text-lg rounded-2xl font-bold px-6 py-2"
-                                type="button"
-                            >
-                                Tillbaka
-                            </button>
-                        </Link>
-                        {uppdrag?.nollk === session?.user.nollk ?
-                            <Link
-                                    href={{
-                                        pathname: '/uppdrag/edituppdrag/[id]',
-                                        query: { id: uppdrag?.id }
-                                    }}
-                            >
-                                <button
-                                    className="h-[44px] w-[125px] bg-mk-blue hover:bg-mk-blue-hover text-white text-lg rounded-2xl font-bold px-6 py-2"
-                                    type="button"
-                                >
-                                    Redigera
-                                </button>
-                            </Link>
+                    <div className="row-start-3 col-start-1 col-span-7 font-bold text-xl text-left px-2"> Motivering:
+                        <p className="font-normal">
+                            {uppdrag?.motivation}
+                        </p>
+                    </div>
+                    </div>
+                    <div>
+                        {isMK ? (<UppdragComment/>) :
+                            (
+                                <div className="flex gap-9 col-start-1 col-span-2 ">
+                                    <Link href="/home">
+                                    <button
+                                        className="h-[44px] w-[125px] bg-mk-yellow hover:bg-mk-yellow-hover text-white text-lg rounded-2xl font-bold px-6 py-2"
+                                        type="button"
+                                    >
+                                        Tillbaka
+                                    </button>
+                                </Link>
+                                {uppdrag?.nollk === session?.user.nollk ?
+                                    <Link href={{
+                                            pathname: '/uppdrag/edituppdrag/[id]',
+                                            query: { id: uppdrag?.id } }}>
+                                        <button
+                                            className="h-[44px] w-[125px] bg-mk-blue hover:bg-mk-blue-hover text-white text-lg rounded-2xl font-bold px-6 py-2"
+                                            type="button">
+                                            Redigera
+                                        </button>
+                                    </Link>
 
-                        : <p></p>}
-
-                    </div>)}
+                                : <p></p>
+                                }
+                                </div>
+                            )
+                        }
+                    </div>
                 </div>
             </MainPage>
         </>
