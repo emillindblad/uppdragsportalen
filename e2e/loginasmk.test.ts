e5986cb54e911ab0d97ae9d17f56a7516199d342
angelixa <julia.bockert@outlook.com>
2023-03-13 14:19:11 +0100

    started testing

--- e2e/loginasmk.test.ts
+++ e2e/loginasmk.test.ts
@@ -24,3 +24,24 @@ test('login as mk', async ({ page }) => {
     await page.getByRole('button', { name: 'Konton' }).click();
     await expect(page).toHaveURL('/accounts');
 });
+
+test('view specific uppdrag', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.locator('input[name="email"]').click();
+    await page.locator('input[name="email"]').fill('pr@mk.chs.chalmers.se');
+    await page.locator('input[name="email"]').press('Tab');
+    await page.getByRole('link', { name: 'Glömt lösenord?' }).press('Tab');
+    await page.locator('input[name="password"]').fill('1234');
+    await page.getByRole('button', { name: 'Logga in' }).click();
+
+    await page.goto('http://localhost:3000/review');
+    await expect(page).toHaveURL('/review');
+
+    await page.getByRole('link', {name : 'Komma på dessa fake uppdrag'}).click();
+    await
+    
+
+
+
+
+});
