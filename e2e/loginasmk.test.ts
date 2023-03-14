fd0919b2126c7c86e2af669edf12dbd6d2449970
emillindblad <emil@lindblad.tech>
2023-03-14 21:35:53 +0100

    fix: optimised e2e tests

--- e2e/loginasmk.test.ts
+++ e2e/loginasmk.test.ts
@@ -1,62 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('login page', async ({ page }) => {
-    await page.goto('/login');
+test.describe.parallel('logged in as mk', () => {
+    test.use({storageState: './e2e/setup/mk.json'})
+        test('Login as mk', async ({ page, context }) => {
+            await page.goto('/review');
+            await expect(page).toHaveURL('/review');
+            await expect(page.getByRole('button', { name: 'Konton' })).toHaveText('Konton');
+            await expect(page.getByRole('button', { name: 'Granska' })).toHaveText('Granska');
 
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle("Uppdragsportalen - Mottagningskommittén");
-    await expect(page.getByRole('button', { name: 'Logga in' })).toHaveText('Logga in');
-});
+            await page.getByRole('button', { name: 'Konton' }).click();
+            await expect(page).toHaveURL('/accounts');
+        });
+        test('View specific uppdrag as MK', async ({ page, context }) => {
+            await page.goto('/archive')
+            await page.getByText('Komma på dessa fake uppdrag').click();
+            await expect(page.getByRole('button', { name: 'Skicka' })).toHaveText('Skicka');
+        });
 
-test('Login as mk', async ({ page }) => {
-    await page.goto('http://localhost:3000/login');
-    await page.locator('input[name="email"]').click();
-    await page.locator('input[name="email"]').fill('pr@mk.chs.chalmers.se');
-    await page.locator('input[name="email"]').press('Tab');
-    await page.getByRole('link', { name: 'Glömt lösenord?' }).press('Tab');
-    await page.locator('input[name="password"]').fill('1234');
-    await page.getByRole('button', { name: 'Logga in' }).click();
-
-    await expect(page).toHaveURL('/review');
-    await expect(page.getByRole('button', { name: 'Konton' })).toHaveText('Konton');
-    await expect(page.getByRole('button', { name: 'Granska' })).toHaveText('Granska');
-
-    await page.getByRole('button', { name: 'Konton' }).click();
-    await expect(page).toHaveURL('/accounts');
-});
-
-test('View specific uppdrag as MK', async ({ page }) => {
-    await page.goto('http://localhost:3000/login');
-    await page.locator('input[name="email"]').click();
-    await page.locator('input[name="email"]').fill('pr@mk.chs.chalmers.se');
-    await page.locator('input[name="email"]').press('Tab');
-    await page.getByRole('link', { name: 'Glömt lösenord?' }).press('Tab');
-    await page.locator('input[name="password"]').fill('1234');
-    await page.getByRole('button', { name: 'Logga in' }).click();
-
-    await expect(page).toHaveURL('/review');
-    await page.getByText('Komma på dessa fake uppdrag').click();
-    await expect(page.getByRole('button', { name: 'Skicka' })).toHaveText('Skicka');
-    
-    await page.getByRole('button', { name: 'Konton' }).click();
-    await expect(page).toHaveURL('/accounts');
-
-
-
-});
-
-test('See account page as MK', async ({ page }) => {
-    await page.goto('http://localhost:3000/login');
-    await page.locator('input[name="email"]').click();
-    await page.locator('input[name="email"]').fill('pr@mk.chs.chalmers.se');
-    await page.locator('input[name="email"]').press('Tab');
-    await page.getByRole('link', { name: 'Glömt lösenord?' }).press('Tab');
-    await page.locator('input[name="password"]').fill('1234');
-    await page.getByRole('button', { name: 'Logga in' }).click();
-    
-    await page.getByRole('button', { name: 'Konton' }).click();
-    await expect(page).toHaveURL('/accounts');
-
-
-
-});
+        test('See account page as MK', async ({ page, context }) => {
+            await page.goto('/accounts')
+            await expect(page).toHaveURL('/accounts');
+        });
+})
